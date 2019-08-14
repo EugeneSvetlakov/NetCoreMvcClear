5827f2609b0a9d2ef5cc689200a8ff7f5827e0e9
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-08-14 15:49:03 +0300

    Категории товаров (отображенеи на сайт)

--- NetCoreMvcClear/Controllers/RentItemController.cs
+++ NetCoreMvcClear/Controllers/RentItemController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetCoreMvcClear.Data.Interfaces;
+using NetCoreMvcClear.Data.Models;
 using NetCoreMvcClear.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -19,14 +20,41 @@ namespace NetCoreMvcClear.Controllers
             _Categories = iCategories;
         }
 
-        public ViewResult List()
+        [Route("RentItem/List")]
+        [Route("RentItem/List/{category}")]
+        public ViewResult List(string category)
         {
-            ViewBag.Title = "Страница со списком проката";
-            RentItemViewModel obj = new RentItemViewModel();
-            obj.GetAllRentItems = _RentItems.AllRentItems;
-            obj.currentCategory = "список единиц:";
+            string _category = category;
+            IEnumerable<RentItem> rentItems;
+            string currCategory = "";
 
-            return View(obj);
+            if (string.IsNullOrEmpty(category))
+            {
+                rentItems = _RentItems.AllRentItems.OrderBy(i => i.Id);
+            }
+            else
+            {
+                if(string.Equals("skyboots", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    rentItems = _RentItems.AllRentItems.Where(i => i.Category.Name.Equals("Ботинки Лыжные")).OrderBy(i => i.Id);
+                    currCategory = "Ботинки Лыжные";
+                }
+                else
+                {
+                    rentItems = _RentItems.AllRentItems.Where(i => i.Category.Name.Equals("Лыжи")).OrderBy(i => i.Id);
+                    currCategory = "Лыжи";
+                }
+            }
+
+            var rentObj = new RentItemViewModel
+            {
+                GetAllRentItems = rentItems,
+                currentCategory = currCategory
+            };
+
+            ViewBag.Title = "На прокат";
+            
+            return View(rentObj);
         }
     }
 }
--- NetCoreMvcClear/Startup.cs
+++ NetCoreMvcClear/Startup.cs
@@ -53,7 +53,13 @@ namespace NetCoreMvcClear
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession();
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{Id?}");
+                routes.MapRoute(name: "categoryFilter", template: "RentItem/{action}/{category?}", defaults: new { Controller = "RentItem", action = "List" });
+
+            });
 
             using (var scope = app.ApplicationServices.CreateScope())
             {
